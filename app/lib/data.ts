43bf31cbb793920d72097abab880df172387c5fd
Amit Timalsina <amittimalsina14@gmail.com>
2024-12-24 21:02:22 +0530

    Add metadata

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,7 +17,6 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await client.sql<Revenue>`SELECT * FROM revenue`;
 
@@ -33,8 +32,6 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
     console.log('Fetching latest invoices data...');
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
     const data = await client.sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
