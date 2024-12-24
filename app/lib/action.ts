59d66f08cc946d748985aefcf393f7e8b1573b98
Amit Timalsina <amittimalsina14@gmail.com>
2024-12-24 21:09:38 +0530

    Fix lint errors

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -88,7 +88,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
-    return { message: 'Database Error: Failed to Update Invoice.' };
+    return { message: `Database Error ${error}: Failed to Update Invoice.` };
   }
  
   revalidatePath('/dashboard/invoices');
